b09f534d50a385bbe228948efc017c08bcd0b91b
Jonas Herzig <jonas@spark-squared.com>
2024-04-25 10:31:39 +0200

    Support Kotlin Gradle Plugin 1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
-    implementation("com.github.replaymod:remap:3a12dc2")
+    implementation("com.github.replaymod:remap:78d2645")
     implementation("net.fabricmc:tiny-mappings-parser:0.2.1.13")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
